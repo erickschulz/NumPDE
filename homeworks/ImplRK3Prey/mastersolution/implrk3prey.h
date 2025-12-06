af07b9d87ec62a81c98cc6f3c6ae4f57397d189f
Erick Schulz <schulz@plexim.com>
2025-12-06 19:01:49 +0100

    fixing double linking errors

--- homeworks/ImplRK3Prey/mastersolution/implrk3prey.h
+++ homeworks/ImplRK3Prey/mastersolution/implrk3prey.h
@@ -21,7 +21,7 @@ namespace ImplRK3Prey {
 // Compute the Kronecker product $C = A \otimes B$
 // A is m x n matrix, B is l x k matrix
 // return Kronecker product of A and B: dim is m*l x n*k
-Eigen::MatrixXd kron(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B) {
+inline Eigen::MatrixXd kron(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B) {
   Eigen::MatrixXd C(A.rows() * B.rows(), A.cols() * B.cols());
   for (unsigned int i = 0; i < A.rows(); ++i) {
     for (unsigned int j = 0; j < A.cols(); ++j) {
